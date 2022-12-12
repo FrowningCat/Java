9123f8779bcd2e14d9ccb734cfc1f10ad34b3651
FrowningCat <vsavinkov60@gmail.com>
2022-12-12 17:24:07 +0300

    add homework

--- src/lesson6/homework.java
+++ src/lesson6/homework.java
@@ -1,7 +1,29 @@
 package lesson6;
 
+import java.util.Random;
+import java.util.TreeSet;
+import java.util.Set;
+
 public class homework {
+
+    public static set sett = new set();
+
+//    Объявить и инициализировать компаратор с обратной (от большего к меньшеиу) логикой сортировки чисел. -
+
     public static void main(String[] args) {
+
+        //Объявить и инициализировать TreeSet использующий ваш компаратор.
+        Set<Integer> treeSet = new TreeSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            int a = new Random().nextInt(1000);
+            sett.methodAdd(a);
+            treeSet.add(a);
+        }
+
+        System.out.println(sett.methodRead());
+        //Добавить несколько эллементов в сет и вывести в консоль.
+        System.out.println(treeSet);
 
     }
 }
--- src/lesson6/set.java
+++ src/lesson6/set.java
@@ -1,16 +1,16 @@
 package lesson6;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class set {
-    private Map<Integer, Object> mapInt = new HashMap<>();
+    //Реализовать множество (set) с помощью TreeMap.
+    private Map<Integer, Object> mapInt = new TreeMap<>();
 
     private static final Object SOMETHING = new Object();
 
+    //Создать метод add, добавляющий элемент в ваше множество.
     public void methodAdd(int x){ mapInt.put(x, SOMETHING); }
-
-    public int methodSize() { return mapInt.size();}
 
     public String methodRead(){ return mapInt.keySet().toString(); }
 }
